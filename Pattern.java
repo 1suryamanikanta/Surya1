973cd4245bb8f69087191e3f02b5d80ed897e2ae
M.surya manikanta <71427437+1suryamanikanta@users.noreply.github.com>
2021-07-01 22:02:58 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+class pattern
+{
+public static void main(String args[])
+{
+int i,j;
+int rows=5;
+char ch='A';
+for(i=1;i<=rows;++i)
+{
+for(j=1;j<=i;++j)
+{
+System.out.print(ch+" ");
+ch++;
+}
+System.out.println();
+}
+}
+} 
